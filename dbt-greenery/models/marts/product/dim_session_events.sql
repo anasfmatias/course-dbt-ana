970c37c252d3d9f4ceffc806299f1cc1c9f9f1aa
Ana Matias <anasfmatias@gmail.com>
2021-12-16 08:50:51 +0000

    week 4

--- dbt-greenery/models/marts/product/dim_session_events.sql
+++ dbt-greenery/models/marts/product/dim_session_events.sql
@@ -1,27 +1,41 @@
+
 {{
   config(
-    materialized='table'
+    materialized='view'
   )
 }}
 
-{%
-    set events_types = [
-        'delete_from_cart'
-        , 'checkout'
-        , 'page_view'
-        , 'add_to_cart'
-        , 'package_shipped'
-        , 'account_created'
-        , 'referral'
-    ]
+{% set event_types = [ 
+    'delete_from_cart'
+    ,'checkout'
+    ,'page_view'
+    ,'add_to_cart'
+    ,'package_shipped'
+    ,'account_created'
+]
 %}
 
-SELECT
-    session_guid
-    , user_guid
-    {% for event_type in event_types %}
-    , sum(case when event_type = '{{event_type}}' then 1 else 0 end) AS {{event_type}}
-    {% endfor %}
-FROM {{ ref('stg_events')}}
+WITH all_events AS (
 
-{{dbt_utils.group_by(2)}}
+                SELECT  *
+                FROM {{ref('stg_events')}} 
+                WHERE events_created_at_utc IS NOT NULL )
+
+, products AS ( SELECT  *
+                FROM {{ref('stg_products')}}  )
+
+, first_user AS (SELECT 
+                      DISTINCT session_guid
+                      , FIRST_VALUE(user_guid) OVER (PARTITION BY session_guid ORDER BY events_created_at_utc ROWS between unbounded preceding and unbounded following ) as user_guid -- sessions have multiple users ( data qyality issue)
+                FROM all_events AS ev)
+SELECT 
+       ev.session_guid
+     , events_created_at_utc::date as event_date
+     , u.user_guid 
+       {%for event_type in event_types %}
+     , SUM(CASE WHEN event_type = '{{event_type}}' THEN 1 ELSE 0 END) as {{event_type}}_events
+      {%endfor%}
+
+FROM all_events AS ev
+JOIN first_user u ON ev.session_guid = u.session_guid
+{{dbt_utils.group_by(3)}}
